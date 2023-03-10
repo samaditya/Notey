12e1719999e87e4e6f36c0b678a1274a38a6a54b
samaditya jatar <samadityaj@gmail.com>
2023-03-10 11:55:40 +0530

    Routing Working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import ErrorBoundary from './components/ErrorBoundary'
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import { BrowserRouter, Routes ,Route} from 'react-router-dom';
-
+import "./App.css";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <ErrorBoundary>
-    <BrowserRouter>
-     <Navbar/>
-     <Routes>
-     <Route exact path="/" element={<Home/>}/>
-     <Route exact path="/about" element={<About/>}/>
-     </Routes>
-     </BrowserRouter>
-
-     </ErrorBoundary>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" key={Home} element={ <Home/> } />
+            <Route exact path="/about" key={About} element={ <About/> } />
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,43 @@
 // import PropTypes from 'prop-types'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () =>{
-  
-    return (
-        <>
-        
-  <nav className="navbar fixed-top navbar-expand-lg navbar-dark navbar-custom" style={{backgroundColor:"#702BF6",color:"white"}}>
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Newsman</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
-    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        
-        <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-
-      </ul>
-     
-    </div>
-  </div>
-</nav>
-
-        </>
-    )
-}
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Newsman
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 export default Navbar;
